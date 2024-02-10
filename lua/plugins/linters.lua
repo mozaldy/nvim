0830aa6e60cefef78eb2d28600aaf1f0a734cc6a
mozaldy <moxaldy@gmail.com>
2024-02-10 16:23:02 +0700

    update linters

--- lua/plugins/linters.lua
+++ lua/plugins/linters.lua
@@ -4,12 +4,12 @@ return {
   opts = {
     events = { "BufWritePost", "BufReadPost", "InsertLeave" },
     linters_by_ft = {
-      -- javascript = { "eslint_d" },
-      -- typescript = { "eslint_d" },
-      -- javascriptreact = { "eslint_d" },
-      -- typescriptreact = { "eslint_d" },
-      -- svelte = { "eslint_d" },
-      -- python = { "pylint" },
+      javascript = { "eslint_d" },
+      typescript = { "eslint_d" },
+      javascriptreact = { "eslint_d" },
+      typescriptreact = { "eslint_d" },
+      svelte = { "eslint_d" },
+      python = { "pylint" },
       htmldjango = { "djlint" },
     },
   },
